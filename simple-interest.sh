643036c91dd6acdb66c8afb21d547d391b35211a
trynosmzondo <101430176+trynosmzondo@users.noreply.github.com>
2022-08-19 17:52:21 +0200

    changes simple-interest-sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,24 +5,24 @@
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+# Additional Authors:
+# trynosmzondo
 
-   # Input:
-   # p, principal amount
-   # t, time period in years
-   # r, annual rate of interest
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
 
-   # Output:
-   # simple interest = p*t*r
+# Output:
+# simple interest = p*t*r
 
-   echo "Enter the principal:"
-   read p
-   echo "Enter rate of interest per year:"
-   read r
-   echo "Enter time period in years:"
-   read t
+echo "Enter the principal:"
+read p
+echo "Enter rate of interest per year:"
+read r
+echo "Enter time period in years:"
+read t
 
-   s=`expr $p \* $t \* $r / 100`
-   echo "The simple interest is: "
-   echo $s
+s=`expr $p \* $t \* $r / 100`
+echo "The simple interest is: "
+echo $s
