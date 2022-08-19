161220d3831119f4da856c71455ecdadb1f7c4ce
trynosmzondo <101430176+trynosmzondo@users.noreply.github.com>
2022-08-19 17:47:08 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-   # This script calculates simple interest given principal,
-   # annual rate of interest and time period in years.
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
 
-   # Do not use this in production. Sample purpose only.
+# Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+# Author: Upkar Lidder (IBM)
    # Additional Authors:
    # <your GitHub username>
 
